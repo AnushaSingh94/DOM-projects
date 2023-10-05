c481f61816b70773a2f9752eb860730b4e88a7b0
Anusha Singh <126671949+AnushaSingh94@users.noreply.github.com>
2023-10-05 12:39:31 +0530

    Click on folder

---
+++ Event listener one click/script.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box");
+
+// now add a click event listner to the element
+
+boxElement.addEventListener("click", function (){
+    alert("got it, you just click me 👍");
+});
